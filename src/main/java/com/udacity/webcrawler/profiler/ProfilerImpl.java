31b27710ca688051464f5ae249d886397fa29e0b
“Luisa <xtremelu@gmail.com>
2023-05-07 17:16:06 +0100

    Profiler

--- src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
+++ src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
@@ -3,50 +3,94 @@ package com.udacity.webcrawler.profiler;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.Proxy;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Clock;
 import java.time.ZonedDateTime;
+import java.util.Arrays;
 import java.util.Objects;
 
 import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
 
 /**
- * Concrete implementation of the {@link Profiler}.
+ * Concrete implementation of the {@link Profiler} interface.
+ * The {@code ProfilerImpl} provides methods for wrapping objects with profiling capabilities
+ * and writing profiling data to a file or writer.
  */
 final class ProfilerImpl implements Profiler {
 
-  private final Clock clock;
-  private final ProfilingState state = new ProfilingState();
-  private final ZonedDateTime startTime;
+    private final Clock clock;
+    private final ZonedDateTime startTime;
+    private final ProfilingState state = new ProfilingState();
 
-  @Inject
-  ProfilerImpl(Clock clock) {
-    this.clock = Objects.requireNonNull(clock);
-    this.startTime = ZonedDateTime.now(clock);
-  }
+    /**
+     * Constructs a {@code ProfilerImpl} with the provided clock.
+     *
+     * @param clock the clock used for profiling.
+     */
+    @Inject
+    ProfilerImpl(Clock clock) {
+        this.clock = Objects.requireNonNull(clock, "Clock must not be null");
+        this.startTime = ZonedDateTime.now(clock);
+    }
 
-  @Override
-  public <T> T wrap(Class<T> klass, T delegate) {
-    Objects.requireNonNull(klass);
+    @Override
+    public <T> T wrap(Class<T> klass, T delegate) {
+        Objects.requireNonNull(klass, "Class must not be null");
+        validateProfiledMethods(klass);
 
-    // TODO: Use a dynamic proxy (java.lang.reflect.Proxy) to "wrap" the delegate in a
-    //       ProfilingMethodInterceptor and return a dynamic proxy from this method.
-    //       See https://docs.oracle.com/javase/10/docs/api/java/lang/reflect/Proxy.html.
+        Object proxy = Proxy.newProxyInstance(
+                ProfilerImpl.class.getClassLoader(),
+                new Class[]{klass},
+                (proxyObj, method, args) ->
+                        new ProfilingMethodInterceptor(clock, delegate, state, startTime)
+                                .invoke(proxyObj, method, args)
+        );
 
-    return delegate;
-  }
+        return klass.cast(proxy);
+    }
 
-  @Override
-  public void writeData(Path path) {
-    // TODO: Write the ProfilingState data to the given file path. If a file already exists at that
-    //       path, the new data should be appended to the existing file.
-  }
+    /**
+     * Validates if the given class has any methods annotated with {@link Profiled}.
+     *
+     * @param klass the class to check for profiled methods.
+     * @throws IllegalArgumentException if the class does not have any profiled methods.
+     */
+    private void validateProfiledMethods(Class<?> klass) {
+        boolean hasProfiledMethods = hasProfiledMethods(klass);
+        if (!hasProfiledMethods) {
+            throw new IllegalArgumentException(klass.getName() + " must have profiled methods.");
+        }
+    }
 
-  @Override
-  public void writeData(Writer writer) throws IOException {
-    writer.write("Run at " + RFC_1123_DATE_TIME.format(startTime));
-    writer.write(System.lineSeparator());
-    state.write(writer);
-    writer.write(System.lineSeparator());
-  }
+    /**
+     * Checks if the given class has any methods annotated with {@link Profiled}.
+     *
+     * @param klass the class to check for profiled methods.
+     * @return true if the class has profiled methods, false otherwise.
+     */
+    private boolean hasProfiledMethods(Class<?> klass) {
+        return Arrays.stream(klass.getDeclaredMethods())
+                .anyMatch(method -> method.isAnnotationPresent(Profiled.class));
+    }
+
+    @Override
+    public void writeData(Path path) {
+        Objects.requireNonNull(path, "Path must not be null");
+
+        try (Writer writer = Files.newBufferedWriter(path)) {
+            writeData(writer);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void writeData(Writer writer) throws IOException {
+        writer.write("Run at " + RFC_1123_DATE_TIME.format(startTime));
+        writer.write(System.lineSeparator());
+        state.write(writer);
+        writer.write(System.lineSeparator());
+    }
 }
--- src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
+++ src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
@@ -1,8 +1,12 @@
 package com.udacity.webcrawler.profiler;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZonedDateTime;
 import java.util.Objects;
 
 /**
@@ -11,20 +15,70 @@ import java.util.Objects;
  */
 final class ProfilingMethodInterceptor implements InvocationHandler {
 
-  private final Clock clock;
+    private final Clock clock;
+    private final Object delegate;
+    private final ZonedDateTime startTime;
+    private final ProfilingState state;
 
-  // TODO: You will need to add more instance fields and constructor arguments to this class.
-  ProfilingMethodInterceptor(Clock clock) {
-    this.clock = Objects.requireNonNull(clock);
-  }
+    /**
+     * Constructs a {@link ProfilingMethodInterceptor} with the provided dependencies.
+     *
+     * @param clock     the clock used for measuring time
+     * @param delegate  the delegate object being profiled
+     * @param state     the profiling state to record method durations
+     * @param startTime the start time of the profiling
+     */
+    ProfilingMethodInterceptor(Clock clock, Object delegate, ProfilingState state, ZonedDateTime startTime) {
+        this.clock = Objects.requireNonNull(clock, "Clock must not be null");
+        this.delegate = delegate;
+        this.state = state;
+        this.startTime = startTime;
+    }
 
-  @Override
-  public Object invoke(Object proxy, Method method, Object[] args) {
-    // TODO: This method interceptor should inspect the called method to see if it is a profiled
-    //       method. For profiled methods, the interceptor should record the start time, then
-    //       invoke the method using the object that is being profiled. Finally, for profiled
-    //       methods, the interceptor should record how long the method call took, using the
-    //       ProfilingState methods.
-    return null;
-  }
+    /**
+     * Intercepts the method invocation and performs profiling if the method is annotated with {@link Profiled}.
+     * The method is invoked on the delegate object, and the duration of the invocation is recorded in the
+     * {@link ProfilingState}.
+     *
+     * @param proxy  the proxy object
+     * @param method the method being invoked
+     * @param args   the arguments to the method
+     * @return the result of the method invocation
+     * @throws Throwable if an error occurs during the method invocation
+     */
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        // Determine whether the method is annotated with the profiled annotation
+        boolean isProfiled = method.isAnnotationPresent(Profiled.class);
+        // Invoke the method with profiling if profiled, otherwise invoke it without profiling
+        return isProfiled ? profiledInvocation(method, args) : method.invoke(delegate, args);
+    }
+
+
+    // Auxiliary method that performs a profile invocation on the delegate object
+    private Object profiledInvocation(Method method, Object[] args) throws Throwable {
+        // Get the current instant as the start time for profiling
+        Instant start = clock.instant();
+
+        try {
+            // Invoke the method on the delegate object, and return the result of the method invocation
+            Object result = method.invoke(delegate, args);
+            return result;
+        } catch (InvocationTargetException ex) {
+            // Throw the original exception that was thrown by the method
+            throw ex.getTargetException();
+        } finally {
+            // Finally, record the duration
+            recordDuration(method, start);
+        }
+    }
+
+    // Auxiliary method to record the duration of the method invocation on the delegate object
+    private void recordDuration(Method method, Instant start) {
+        // Calculate the duration of the method invocation
+        Duration duration = Duration.between(start, clock.instant());
+        // Record the duration in the profiling state
+        state.record(delegate.getClass(), method, duration);
+    }
+
 }
